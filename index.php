9426214b03598f762ee6a966ae7be7e46415094e
LucasDeluge <deluge999@gmail.com>
2022-08-03 14:34:13 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>L'île de Solidarité</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<header>
+</header>
+<div>
+<h1>Présentation de l'association</h1>
+    <p>
+L’île de solidarité intervient auprés des sans abris,sans domiciles fixes et réfugiés.
+Apolitique et non confessionnelle, elle est ouverte à toute personne sans distinction culturelle, générationnelle et sociale.
+Nos principales actions sont les maraudes sociales dans lesquelles sont apportées une aide alimentaire, vestimentaire et une orientation vers les différents dispositifs d’accueil et d’hébergement.
+Forte d’un nombre important de bénévoles, elle développe des liens sociaux avec les personnes à la rue afin de travailler sur leur estime de soi et de les faire participer à des projets d’inclusion qu’ils soient sportifs,culturels ou d’insertion.
+Composée uniquement de bénévoles, l’île de solidarité a su trouver sa place auprès des instances et aujourd’hui elle fait partie des conseils d’administrations du Samu social et de la Fédération des Acteurs de la Solidarité.
+Afin de rester fidèle a ses valeurs humaines, nous comptons beaucoup sur vous pour nous aider à continuer notre combat.
+    </p>
+    </div>
+</body>
+</html>
