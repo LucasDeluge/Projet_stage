425703e7b4f6e42bcf1c9ca0283b2ac4498d1932
Your Name <courbot.theo@gmail.com>
2022-08-06 12:07:36 +0200

    page des dons

--- donation.php
+++ donation.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>L'île de Solidarité actus</title>
+  <title>L'île de Solidarité Dons</title>
   <link rel="stylesheet" href="./assets/styles.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   <link rel="stylesheet" href="./assets/bootstrap.min.css">
@@ -51,8 +51,50 @@
 </header>
 
 <body>
-  <h1>test don</h1>
+  <h1>faite un don</h1>
+  <p>Vous pouvez nous aider!
+
+    Afin de nous aider dans nos actions, vous pouvez nous faire un don financier!
+
+    Vous recevrez si vous le souhaitez un reçu pour déduction fiscale.
+    100€ de dons vous reviendra au final à 34€ après déduction fiscale.</p>
 </body>
+
+<ul>50€ => 17€ après déduction fiscale.</ul>
+<ul>30€ => 10,20€ après déduction fiscale.</ul>
+<ul>20€ => 6,80€ après déduction fiscale.</ul>
+<ul>10€ => 3,40€ après déduction fiscale.</ul>
+
+<p>Les dons au profit de l’association L’Île de Solidarité ouvrent droit à une réduction d’impôt sur le revenu de 66 % du montant versé, dans la limite de 20 % du revenu imposable.
+  L’association L’Île de Solidarité est une association d’intérêt général qui est habilité à éditer un reçu fiscal que vous recevrez automatiquement suite à votre don.</p>
+
+
+<p>Vous pouvez effectuer un don en cliquant
+  <a href="https://www.helloasso.com/associations/l-ile-de-solidarite">ici</a>
+</p>
+
+<p>
+  Vous pouvez également nous envoyer des dons par chèque.
+  Adresse et ordre :
+</p>
+
+<ul> L’île de solidarité </ul>
+<ul> 13 rue de rivoli </ul>
+<ul> 59800 LILLE-fives </ul>
+<ul> Merci de votre générosité ! </ul>
+
+
+commantaire marche pas
+chercher a inserer la video
+
+<h1> Usage des derniers dons : </h1>
+
+<p> Achat d’un camion pour collecter les denrées alimentaires auprès des donateurs, commerces, etc .. et acheminer ces denrées sur les lieux de distributions une fois conditionnées ou préparées. </p>
+
+
+
+
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 
 </html>
