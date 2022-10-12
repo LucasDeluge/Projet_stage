8e2d67194c1eee0d2e96959a038aedb93aadec31
LucasDeluge <deluge999@gmail.com>
2022-10-12 14:48:46 +0200

    modif

--- index.php
+++ index.php
@@ -79,9 +79,12 @@
     </p>
     </div>
 
+    <footer>
     <div class="logo">
   <img src="./assets/logoids-modified.png">
 </div>
+</footer>
+
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 
--- partenariat.php
+++ partenariat.php
@@ -74,8 +74,10 @@
 
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-<div class="logo">
+<footer>
+    <div class="logo">
   <img src="./assets/logoids-modified.png">
 </div>
+</footer>
 
 </html>
